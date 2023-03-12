2f77ff6a888883c20af9c70313be53ca4e6689e2
Mimi <miraclef60@gmail.com>
2023-03-12 21:28:03 +0100

    fix: refactored import statements in files to use correct organization naming

--- packages/api/src/trpc.ts
+++ packages/api/src/trpc.ts
@@ -11,8 +11,8 @@ import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import superjson from "superjson";
 import { ZodError } from "zod";
 
-import { getServerSession, type Session } from "@acme/auth";
-import { prisma } from "@acme/db";
+import { getServerSession, type Session } from "@myrelayer/auth";
+import { prisma } from "@myrelayer/db";
 
 /**
  * 1. CONTEXT
